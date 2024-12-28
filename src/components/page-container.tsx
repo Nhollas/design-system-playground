60bee7708626dbb7d30c2785b31a1df04df17a49
Nick Hollas <nicholashollas@yahoo.co.uk>
2024-12-28 20:18:05 +0000

    Increase maximum width of PageContainer component for better layout on larger screens

--- src/components/page-container.tsx
+++ src/components/page-container.tsx
@@ -1,6 +1,6 @@
 export function PageContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-1 desktop:grid-cols-2 gap-16 desktop:gap-24 max-w-sm desktop:max-w-xl mx-auto">
+    <div className="grid grid-cols-1 desktop:grid-cols-2 gap-16 desktop:gap-24 max-w-sm desktop:max-w-2xl mx-auto">
       {children}
     </div>
   )
